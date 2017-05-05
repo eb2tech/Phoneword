c1b1b9c4456c22bdede2826136646f39bd349290
Edward Blakes <eblakes@eb2technologies.com>
2017-05-04 22:47:36 -0500

    Completed code entry

--- Phoneword.droid/MainActivity.cs
+++ Phoneword.droid/MainActivity.cs
@@ -1,10 +1,11 @@
 ﻿using Android.App;
+using Android.Content;
 using Android.Widget;
 using Android.OS;
 
 namespace Phoneword.droid
 {
-    [Activity(Label = "Phoneword.droid", MainLauncher = true, Icon = "@drawable/icon")]
+    [Activity(Label = "Phone Word", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
         protected override void OnCreate(Bundle bundle)
@@ -12,7 +13,46 @@ namespace Phoneword.droid
             base.OnCreate(bundle);
 
             // Set our view from the "main" layout resource
-            // SetContentView (Resource.Layout.Main);
+            SetContentView (Resource.Layout.Main);
+
+            var phoneNumberText = FindViewById<EditText>(Resource.Id.PhoneNumberText);
+            var translateButton = FindViewById<Button>(Resource.Id.TranslateButton);
+            var callButton = FindViewById<Button>(Resource.Id.CallButton);
+
+            callButton.Enabled = false;
+
+            var translatedNumber = string.Empty;
+
+            translateButton.Click += (sender, e) =>
+            {
+                // Translate user's alphanumeric phone number to numeric 
+                translatedNumber = PhoneTranslator.ToNumber(phoneNumberText.Text);
+                if (string.IsNullOrWhiteSpace(translatedNumber))
+                {
+                    callButton.Text = "Call";
+                    callButton.Enabled = false;
+                }
+                else
+                {
+                    callButton.Text = "Call " + translatedNumber;
+                    callButton.Enabled = true;
+                }
+            };
+
+            callButton.Click += (sender, e) =>
+            {
+                var callDialog = new AlertDialog.Builder(this);
+                callDialog.SetMessage("Call " + translatedNumber + "?");
+                callDialog.SetNeutralButton("Call", (o, args) =>
+                {
+                    var callIntent = new Intent(Intent.ActionCall);
+                    callIntent.SetData(Android.Net.Uri.Parse("tel:" + translatedNumber));
+                    StartActivity(callIntent);
+                });
+                callDialog.SetNegativeButton("Cancel", (o, args) => { });
+
+                callDialog.Show();
+            };
         }
     }
 }
--- Phoneword.droid/Resources/Resource.Designer.cs
+++ Phoneword.droid/Resources/Resource.Designer.cs
@@ -9,9 +9,9 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: global::Android.Runtime.ResourceDesignerAttribute("Phoneword.Resource", IsApplication=true)]
+[assembly: global::Android.Runtime.ResourceDesignerAttribute("Phoneword.droid.Resource", IsApplication=true)]
 
-namespace Phoneword
+namespace Phoneword.droid
 {
 	
 	
@@ -60,11 +60,14 @@ namespace Phoneword
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f050003
+			public const int CallButton = 2131034115;
+			
 			// aapt resource value: 0x7f050001
 			public const int PhoneNumberText = 2131034113;
 			
 			// aapt resource value: 0x7f050002
-			public const int button1 = 2131034114;
+			public const int TranslateButton = 2131034114;
 			
 			// aapt resource value: 0x7f050000
 			public const int textView1 = 2131034112;
